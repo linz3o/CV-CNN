29839d5b9e159c53349d1a7d30de8d6a495a769e
omerali010 <o.a.celikci@students.uu.nl>
2025-03-04 12:21:32 +0100

    Training/validation split

--- cnn.py
+++ cnn.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 # Download and load training set
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 
+# Split the training set into training and validation sets
+train_size = int(0.8 * len(trainset))
+val_size = len(trainset) - train_size
+trainset, valset = random_split(trainset, [train_size, val_size])
+
 # Download and load test set
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
